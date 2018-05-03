89871603076046c32342c6a1a0e720be8880d22d
rjw253 <rjw253@cornell.edu>
2018-05-03 17:43:34 -0400

    add ApartmentLaundryStyles

---
+++ Components/Styles/ApartmentLaundryStyles.js
@@ -0,0 +1,163 @@
+
+import React from 'react';
+
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+
+main_screen:{
+
+	position: 'relative',
+	width: 375,
+	height: 667,
+	backgroundColor: '#F2F2F2',
+},
+
+rectangle_header:{
+
+	position: 'absolute',
+	width: 375,
+	height: 70,
+	left: 0,
+	top: -4,
+
+	backgroundColor: '#2D9CDB',
+//box-shadow: 0 4 4 rgba(0, 0, 0, 0.25),
+},
+
+Apartment_Laundry_Room_text:{
+
+	position: 'absolute',
+	width: 313,
+	height: 33,
+	left: 31,
+	top: 17,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 'normal',
+	fontSize: 24,
+	textAlign: 'center',
+
+	color: '#F2F2F2',
+},
+
+QR_code_rectangle:{
+
+	position: 'absolute',
+	width: 241,
+	height: 86,
+	left: 59,
+	top: 334,
+
+	backgroundColor: '#2D9CDB',
+	borderRadius: 5,
+},
+
+apartment_code_rectangle:{
+
+	position: 'absolute',
+	width: 241,
+	height: 80,
+	left: 59,
+	top: 156,
+
+	backgroundColor: '#2D9CDB',
+	borderRadius: 5,
+},
+
+done_rectangle:{
+
+	position: 'absolute',
+	width: 169,
+	height: 53,
+	left: 93,
+	top: 527,
+
+	backgroundColor: '#2D9CDB',
+	borderRadius: 10,
+},
+
+apartment_code_text:{
+
+	position: 'absolute',
+	width: 202,
+	height: 25,
+	left: 75,
+	top: 184,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 'normal',
+	fontSize: 18,
+	textAlign: 'center',
+
+	color: '#E0E0E0',
+},
+
+
+OR_text:{
+
+	position: 'absolute',
+	width: 35,
+	height: 33,
+	left: 160,
+	top: 268,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 'normal',
+	fontSize: 24,
+	textAlign: 'center',
+
+	color: '#828282',
+},
+
+
+Scan QR Code_text:{
+
+	position: 'absolute',
+	width: 123,
+	height: 25,
+	left: 126,
+	top: 364,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 'normal',
+	fontSize: 18,
+	textAlign: 'center',
+
+	color: '#F2F2F2',
+},
+
+corner_box_img:{
+	position: 'absolute',
+	width: 22,
+	height: 22,
+	left: 93,
+	top: 366,
+}
+
+
+done_text:{
+
+	position: 'absolute',
+	width: 47,
+	height: 25,
+	left: 152,
+	top: 543,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 'normal',
+	fontSize: 18,
+	textAlign: 'center',
+
+	color: '#F2F2F2',
+},
