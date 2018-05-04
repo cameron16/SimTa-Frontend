e7f530c1ffde2a083aef65b78a1ee713e0cdbb99
rjw253 <rjw253@cornell.edu>
2018-05-04 11:39:05 -0400

    SimTa Home Screen

---
+++ Components/Styles/SimTaHomeStyles.js
@@ -0,0 +1,348 @@
+import React from 'react'
+
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+/* SimTa Smart Home */
+main_screen:{
+	position: 'relative',
+	width: 375,
+	height: 667,
+
+	backgroundColor: '#FFFFFF',
+},
+
+/* rectangle_header */
+rectangle_header:{
+	position: 'absolute',
+	width: 375,
+	height: 70,
+	left: 0,
+	top: -63+-4,
+
+	backgroundColor: '#27AE60',
+	//box-shadow: 0 4 4 rgba(0, 0, 0, 0.25),
+},
+
+/* Your Appliances_text */
+your_appliances_text:{
+	position: 'absolute',
+	width: 194,
+	height: 33,
+	left: 90,
+	top: -63+17,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 40,
+	fontSize: 24,
+	textAlign: 'center',
+
+	color: '#F2F2F2',
+},
+
+/* LAUNDR_rectangle */
+LAUNDRY_rectangle:{
+	position: 'absolute',
+	width: 320,
+	height: 110,
+	left: 28,
+	top: -63+99,
+
+	backgroundColor: '#27AE60',
+	//box-shadow: inset 0 4 4 rgba(0, 0, 0, 0.25),
+	borderRadius: 19,
+},
+
+/* Rectangle 3_laundrymachine.1 
+
+position: 'absolute',
+width: 40,
+height: 13,
+left: 49,
+top: -63+133,
+
+backgroundColor: #6FCF97,
+border: 1 solid #000000,
+box-sizing: border-box,
+borderRadius: 2,*/
+
+
+/* Rectangle_laundry_body */
+Rectangle_laundry_body:{
+	position: 'absolute',
+	width: 42,
+	height: 52,
+	left: 48,
+	top: -63+130,
+},
+
+/* Ellipse_laundrymahcine 
+
+position: 'absolute',
+width: 24,
+height: 23,
+left: 57,
+top: -63+152,
+
+backgroundColor: #6FCF97,
+border: 1 solid #000000,
+box-sizing: border-box,*/
+
+
+/* LAUNDRY_text */
+LAUNDRY_text:{
+	position: 'absolute',
+	width: 195,
+	height: 50,
+	left: 110,
+	top: -63+137,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 40,
+	fontSize: 36,
+	textAlign: 'center',
+
+	color: '#F2F2F2',
+},
+
+/* DISHWASHER_rectangle */
+DISHWASHER_rectangle:{
+	position: 'absolute',
+	width: 320,
+	height: 110,
+	left: 28,
+	top: -63+514,
+
+	backgroundColor: '#27AE60',
+	//box-shadow: inset 0 4 4 rgba(0, 0, 0, 0.25),
+	borderRadius: 19,
+},
+
+/* Dishwasher_text */
+Dishwasher_text:{
+	position: 'absolute',
+	width: 247,
+	height: 50,
+	left: 89,
+	top: -63+548,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 40,
+	fontSize: 32,
+	textAlign: 'center',
+
+	color: '#F2F2F2',
+},
+
+/* OVEN_rectangle */
+OVEN_rectangle:{
+	position: 'absolute',
+	width: 320,
+	height: 110,
+	left: 28,
+	top: -63+239,
+
+	backgroundColor: '#27AE60',
+	//box-shadow: inset 0 4 4 rgba(0, 0, 0, 0.25),
+	borderRadius: 19,
+},
+
+/* Rectangle 4_oven
+
+position: 'absolute',
+width: 40,
+height: 13,
+left: 61,
+top: -63+270,
+
+backgroundColor: #6FCF97,
+border: 1 solid #000000,
+box-sizing: border-box,
+borderRadius: 2, */
+
+
+/* Rectangle_oven_body */
+Rectangle_oven_body:{
+	position: 'absolute',
+	width: 59,
+	height: 42,
+	left: 50,
+	top: -63+267,
+},
+
+/* Rectangle 3_oven 
+
+position: 'absolute',
+width: 40,
+height: 22,
+left: 61,
+top: -63+292,
+
+backgroundColor: #6FCF97,
+border: 1 solid #000000,
+box-sizing: border-box,
+borderRadius: 2, */
+
+
+/* OVEN_text */
+OVEN_text:{
+	position: 'absolute',
+	width: 120,
+	height: 50,
+	left: 125,
+	top: -63+274,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 40,
+	fontSize: 36,
+	textAlign: 'center',
+
+	color: '#F2F2F2',
+},
+
+/* FRIDGE_rectangle */
+FRIDGE_rectangle:{
+	position: 'absolute',
+	width: 320,
+	height: 110,
+	left: 28,
+	top: -63+378,
+
+	backgroundColor: '#27AE60',
+	//box-shadow: inset 0 4 4 rgba(0, 0, 0, 0.25),
+	borderRadius: 19,
+},
+
+/* Rectangle_fridge_body */
+Rectangle_fridge_body:{
+	position: 'absolute',
+	width: 41,
+	height: 54,
+	left: 50,
+	top: -63+404,
+},
+
+/* Rectangle 3_fridge 
+
+position: 'absolute',
+width: 3,
+height: 54,
+left: 81,
+top: -63+404,
+
+backgroundColor: #6FCF97,
+border: 1 solid #000000,
+box-sizing: border-box,
+
+
+/* Rectangle 4_fridge_handle2
+
+position: 'absolute',
+width: 4,
+height: 7,
+left: 76,
+top: -63+427,
+
+backgroundColor: #6FCF97,
+border: 1 solid #000000,
+box-sizing: border-box,
+borderRadius: 2,
+
+
+/* Rectangle 4_fridge_handle1
+
+position: 'absolute',
+width: 4,
+height: 7,
+left: 85,
+top: -63+427,
+
+backgroundColor: #6FCF97,
+border: 1 solid #000000,
+box-sizing: border-box,
+borderRadius: 2,
+*/
+
+/* FRIDGE_text */
+FRIDGE_text:{
+	position: 'absolute',
+	width: 150,
+	height: 50,
+	left: 120,
+	top: -63+412,
+
+	fontFamily: 'Cochin',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	lineHeight: 40,
+	fontSize: 36,
+	textAlign: 'center',
+
+	color: '#F2F2F2',
+},
+
+/* dishwasher_body */
+dishwasher_body:{
+	position: 'absolute',
+	width: 43,
+	height: 37,
+	left: 47,
+	top: -63+550,
+},
+
+/* dishwasher_line_top 
+
+position: 'absolute',
+width: 48,
+height: 0,
+left: 52,
+top: -63+559.06,
+
+border: 2 solid '#27AE60',
+
+
+/* dishwasher_line_bottom 
+
+position: 'absolute',
+width: 48,
+height: 0,
+left: 52,
+top: -63+583.63,
+
+border: 2 solid '#27AE60',
+
+
+/* dishwasher_circle2 
+
+position: 'absolute',
+width: 4,
+height: 4.03,
+left: 85.16,
+top: -63+550.46,
+
+backgroundColor: '#27AE60',
+border: 1 solid #000000,
+box-sizing: border-box,
+
+
+/* dishwasher_circle1
+
+position: 'absolute',
+width: 4,
+height: 4.03,
+left: 92.47,
+top: -63+550.46,
+
+backgroundColor: '#27AE60',
+border: 1 solid #000000,
+box-sizing: border-box,*/
+
+})
